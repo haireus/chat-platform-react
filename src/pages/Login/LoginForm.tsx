4cb361ea553f0f6e6cc248d4f5c4fcc87fae36cd
haireus <62015534+haireus@users.noreply.github.com>
2022-09-18 11:17:27 +0700

    Add react hook form

--- src/pages/Login/LoginForm.tsx
+++ src/pages/Login/LoginForm.tsx
@@ -1,21 +1,38 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Button, InputContainer, InputField, InputLabel } from "../../styles";
 import styles from "./index.module.scss";
 
 export const LoginForm = () => {
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
   };
   return (
-    <form className={styles.form} onSubmit={onSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <InputContainer>
         <InputLabel htmlFor="email">Email</InputLabel>
-        <InputField type={"email"} autoComplete="off" id="email" />
+        <InputField
+          type={"email"}
+          autoComplete="off"
+          id="email"
+          {...register("email", { required: "Email is required" })}
+        />
       </InputContainer>
 
       <InputContainer className={styles.loginForm}>
         <InputLabel htmlFor="password">Password</InputLabel>
-        <InputField type={"password"} autoComplete="off" id="password" />
+        <InputField
+          type={"password"}
+          autoComplete="off"
+          id="password"
+          {...register("password", { required: "Password is required" })}
+        />
       </InputContainer>
 
       <Button className={styles.button}>Login</Button>
--- src/pages/Register/RegisterForm.tsx
+++ src/pages/Register/RegisterForm.tsx
@@ -1,36 +1,63 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button, InputContainer, InputField, InputLabel } from "../../styles";
+import { useForm } from "react-hook-form";
 import styles from "./index.module.scss";
 export const RegisterForm = () => {
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  console.log(errors);
+  const onSubmit = (data: any) => {
+    console.log(data);
   };
+
   return (
-    <form className={styles.form} onSubmit={onSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <InputContainer>
         <InputLabel htmlFor="email">Email</InputLabel>
-        <InputField type={"email"} id="email" />
+        <InputField
+          type={"email"}
+          id="email"
+          {...register("email", { required: "Email is required" })}
+        />
       </InputContainer>
 
       <section className={styles.nameFieldRow}>
         <InputContainer>
           <InputLabel htmlFor="firstName">First Name</InputLabel>
-          <InputField type={"text"} id="firstName" />
+          <InputField
+            type={"text"}
+            id="firstName"
+            {...register("firstName", { required: "First name is required" })}
+          />
         </InputContainer>
 
         <InputContainer>
           <InputLabel htmlFor="lastName">Last Name</InputLabel>
-          <InputField type={"text"} id="lastName" />
+          <InputField
+            type={"text"}
+            id="lastName"
+            {...register("lastName", { required: "Last name is required" })}
+          />
         </InputContainer>
       </section>
 
       <InputContainer>
         <InputLabel htmlFor="password">Password</InputLabel>
-        <InputField type={"password"} id="password" />
+        <InputField
+          type={"password"}
+          id="password"
+          {...register("password", { required: "Password is required" })}
+        />
       </InputContainer>
 
-      <Button className={styles.button}>Create My Account</Button>
+      <Button className={styles.button} type="submit">
+        Create My Account
+      </Button>
 
       <div className={styles.loginUser}>
         <span>Already have an account ? </span>
